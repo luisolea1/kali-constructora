2fe98923b31f28940f67f2a3624cb9323eb9e52d
Luis Alberto Olea Olea <luisolea653@gmail.com>
2026-06-17 02:26:40 -0600

    Agrega components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import "../../blocks/footer.css";
+
+function Footer() {
+return (
+    <footer className="footer">
+    <div className="footer__content">
+        <h2 className="footer__logo">
+        KALI.
+        </h2>
+
+        <p className="footer__copyright">
+        © 2025 KALI. Todos los derechos reservados.
+        </p>
+    </div>
+    </footer>
+);
+}
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+function Main() {
+    return (
+    <main className="page">
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Services */}
+
+      {/* Projects */}
+
+      {/* Partners */}
+
+      {/* Contact */}
+    </main>
+);
+}
+
+export default Main;
