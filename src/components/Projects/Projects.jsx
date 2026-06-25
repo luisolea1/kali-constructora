bb0ab3df4e5ca11819beed61c885bf1626c438f9
Luis Alberto Olea Olea <luisolea653@gmail.com>
2026-06-25 00:44:53 -0600

    Se agregan imagenes a proyecto primavera

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,6 +7,11 @@ import primavera3 from "../../images/projects/primavera/primavera_3.jpg";
 import primavera4 from "../../images/projects/primavera/primavera_4.jpg";
 import primavera5 from "../../images/projects/primavera/primavera_5.jpg";
 import primavera6 from "../../images/projects/primavera/primavera_6.jpg";
+import primavera7 from "../../images/projects/primavera/primavera_7.jpg";
+import primavera71 from "../../images/projects/primavera/primavera_71.jpg";
+import primavera8 from "../../images/projects/primavera/primavera_8.jpg";
+import primavera9 from "../../images/projects/primavera/primavera_9.jpg";
+import primavera10 from "../../images/projects/primavera/primavera_10.jpg";
 
 function Projects() {
   const projects = [
@@ -22,6 +27,13 @@ function Projects() {
     primavera2,
     primavera3,
     primavera4,
+    primavera5,
+    primavera6,
+    primavera7,
+    primavera71,
+    primavera8,
+    primavera9,
+    primavera10,
   ],
 },
 ];
