88e5f3eca79cb780e3cb1b169507a4a67956e4df
Luis Alberto Olea Olea <luisolea653@gmail.com>
2026-06-29 21:15:01 -0600

    Se agrega proyecto Cancha a la seccion de projects

---
+++ src/data/projects/cancha.js
@@ -0,0 +1,27 @@
+import cancha1 from "../../images/projects/cancha//cancha_1.jpg";
+import cancha2 from "../../images/projects/cancha//cancha_2.jpg";
+import cancha3 from "../../images/projects/cancha//cancha_3.jpg";
+import cancha4 from "../../images/projects/cancha//cancha_4.jpg";
+import cancha5 from "../../images/projects/cancha//cancha_5.jpg";
+import cancha6 from "../../images/projects/cancha//cancha_6.jpg";
+import cancha7 from "../../images/projects/cancha//cancha_7.jpg";
+
+const cancha = {
+title: "Cancha de fútbol 7",
+location: "Huitzilac, Morelos",
+category: "Infraestructura Educativa & Obra Exterior",
+description:
+    "Proyecto integral de diseño y construcción de una cancha de fútbol para el bachillerato CBTA 154. La obra combinó la instalación de pasto sintético de alta resistencia y un sólido cercado perimetral de seguridad. Un espacio de alta durabilidad y bajo mantenimiento integrado armoniosamente con su entorno natural.",
+
+images: [
+    cancha1,
+    cancha2,
+    cancha3,
+    cancha4,
+    cancha5,
+    cancha6,
+    cancha7,
+],
+};
+
+export default cancha;
--- src/data/projects/index.js
+++ src/data/projects/index.js
@@ -1,7 +1,8 @@
 import primavera from './primavera';
 import fogatero from './fogatero'
 import maya from './maya';
+import cancha from './cancha';
 
-const projects = [primavera, fogatero, maya];
+const projects = [primavera, fogatero, maya, cancha];
 
 export default projects;
