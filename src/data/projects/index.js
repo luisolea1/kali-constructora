2e49ad614130763bdb042a8055589d3277e4e3a0
Luis Alberto Olea Olea <luisolea653@gmail.com>
2026-06-30 00:21:33 -0600

    Se agrega proyecto Terraza a la seccion de projects

--- src/data/projects/index.js
+++ src/data/projects/index.js
@@ -2,7 +2,8 @@ import primavera from './primavera';
 import fogatero from './fogatero'
 import maya from './maya';
 import cancha from './cancha';
+import terraza from './terraza';
 
-const projects = [primavera, fogatero, maya, cancha];
+const projects = [primavera, fogatero, maya, cancha, terraza];
 
 export default projects;
---
+++ src/data/projects/terraza.js
@@ -0,0 +1,22 @@
+import terraza1 from "../../images/projects/terraza/terraza_1.jpg";
+import terraza2 from "../../images/projects/terraza/terraza_2.jpg";
+import terraza3 from "../../images/projects/terraza/terraza_3.jpg";
+import terraza4 from "../../images/projects/terraza/terraza_4.jpg";
+
+const terraza = {
+  title: "Terraza y pérgola",
+  location: "Cuernavaca, Morelos",
+  category: "Amenidades Hoteleras",
+  description:
+    "La estructura destaca por una techumbre de teja tradicional soportada por una armadura de acero expuesta, logrando un balance perfecto entre lo rústico y lo contemporáneo. Cuenta con una barra lineal de granito pulido de alta resistencia, gabinetes inferiores metálicos a medida y equipamiento de cocina empotrado, todo integrado de manera armónica. ",
+
+
+  images: [
+    terraza1,
+    terraza2,
+    terraza3,
+    terraza4,
+  ]
+};
+
+export default terraza;
