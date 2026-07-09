3c7ae0cdadc8f943fda6790909cae1a97ef0a204
Luis Alberto Olea Olea <luisolea653@gmail.com>
2026-07-09 11:21:56 -0600

    Actualizacion de imagenes

--- src/data/projects/primavera.js
+++ src/data/projects/primavera.js
@@ -9,7 +9,6 @@ import primavera8 from "../../images/projects/primavera/primavera_8.jpg";
 import primavera9 from "../../images/projects/primavera/primavera_9.jpg";
 import primavera10 from "../../images/projects/primavera/primavera_10.jpg";
 import primavera61 from "../../images/projects/primavera/primavera_61.jpg";
-import primavera71 from "../../images/projects/primavera/primavera_71.jpg";
 import primavera81 from "../../images/projects/primavera/primavera_81.jpg";
 
 
@@ -35,7 +34,6 @@ const primavera = {
     primavera6,
     primavera61,
     primavera7,
-    primavera71,
     primavera8,
     primavera81,
     primavera9,
